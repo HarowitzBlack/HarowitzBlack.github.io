262e1600a0f9d47ff2472e50c4b0dfe9bed775b0
Joel Benjamin <joelbenjamin093@gmail.com>
2017-10-19 19:50:23 +0530

    Add files via upload

--- stuff.js
+++ stuff.js
@@ -1,1 +1,34 @@
 
+
+function blink() {
+   var counter = 0;
+   var f = document.getElementById('greettext');
+   setInterval(function() {
+       if(counter < 10){
+           f.style.display = (f.style.display == 'none' ? '' : 'none');
+       }
+       counter += 1;
+   }, 1000);
+}
+
+
+function toggleModal(){
+
+    var modalBox = document.getElementById('box-modal');
+    var toggleModalBtn = document.getElementById('toggle-modal');
+    console.log(toggleModalBtn);
+    console.log(modalBox);
+
+    toggleModalBtn.onclick = function(){
+        modalBox.style.display = "block";
+    }
+
+    window.onclick = function(event) {
+    if (event.target == modalBox) {
+        modalBox.style.display = "none";
+    }
+}
+
+}
+
+toggleModal();
